8034eb2a813d0bfe4c3162b03b04dd424e3ac15d
MichaelKinc <kinc.michael@gmail.com>
2022-08-22 19:03:36 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
